1d6a2ecce15a7e1fea85cc877f9b58abb152d84c
dataEngineerAnalyst <111156099+dataEngineerAnalyst@users.noreply.github.com>
2023-05-02 22:16:34 -0400

    added readme file

--- fahrenheit_to_celsius.py
+++
@@ -1,5 +0,0 @@
-fahrenheit = 32
-
-celsius = (fahrenheit - 32) / 1.8
-
-print(str(fahrenheit) + " degrees fahrenheit is equal to " + str(celsius) + " degree(s) celsius")
--- full_name.py
+++
@@ -1,7 +0,0 @@
-first_name = "Efe" * 500 # printed 500 times
-print(first_name)
-
-last_name = "cantekin"
-
-full_name = first_name + last_name
-print(full_name)
